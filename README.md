41eec852154803263636bd5a3d0ba6092940a2f0
stream <penolove19.am97@g2.nctu.edu.tw>
2017-06-03 16:06:19 +0800

    exit Readme

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 kaggle container
 python with 3.6
+
+you might need to edit the mount folder in the run_server.sh
+
+```
+#start server.sh (jupyter notebook)
+./run_server.sh
+# now you can try to connecting ip:8888
+
+#stop the server
+./stop_server.sh
+
+```
--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,5 @@
 docker run --net host --name kaggle_docker \
         -v /home/stream/Documents:/root/Documents\
-        -d kaggle/python tail -f /dev/null
+        -d kaggle/python jupyter notebook --no-browser --ip="0.0.0.0"\
+        --NotebookApp.token='' --notebook-dir=/root/
 #sudo ./pipework.sh
